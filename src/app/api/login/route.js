97db250bc7032773d01c9dde775b9b3f58c0c726
Waseem <ajmerwaseem1995@gmail.com>
2025-10-03 11:09:05 +0530

    changes in login page

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -28,8 +28,8 @@ export async function POST(req) {
       const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
+          user: process.env.EMAIL_USER || "waseemabbasi5111@gmail.com",
+          pass: process.env.EMAIL_PASS || "mkiz hbei micz vahf",
         },
       });
 
